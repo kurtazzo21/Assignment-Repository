4fb78bb4c0ef4b94a35f3d7b4fa42cd74f7a9c02
kurtazzo21 <32700203+kurtazzo21@users.noreply.github.com>
2017-12-13 14:01:26 +0100

    version 4
    Created the necessary code for the paddles so one can be controlled by the keyboard and the other by the mouse.

---
+++ Pong v4/Assets/scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string levelname)
+    {
+        Debug.Log("Level1: " + levelname);
+        //loads the scene named levelName
+        SceneManager.LoadScene(levelname);
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
