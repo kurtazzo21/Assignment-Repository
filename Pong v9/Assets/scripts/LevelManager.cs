c8b9da0f8479934d893ddb2c084fb21efd56dfec
kurtazzo21 <32700203+kurtazzo21@users.noreply.github.com>
2017-12-14 23:11:25 +0100

    version 9
    Minor changes done to the appearance of the play space.

---
+++ Pong v9/Assets/scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string levelname)
+    {
+        //loads the scene named levelName
+        SceneManager.LoadScene(levelname);
+    }
+
+    public void QuitGame()
+    {
+        //when the button quit is pressed the game closes
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+    public void LoadNextLevel()
+    {
+        //when the button start is pressed level 1 is loaded
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
