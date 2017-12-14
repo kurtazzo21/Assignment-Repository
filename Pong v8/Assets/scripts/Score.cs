aa4dcaee612dc13093c792f5a407949f1c3c0f0b
kurtazzo21 <32700203+kurtazzo21@users.noreply.github.com>
2017-12-14 22:31:08 +0100

    version 8
    Polished the mechanics of the game.

---
+++ Pong v8/Assets/scripts/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Text Scoreboard;
+
+    public static int Paddle1Score = 0;
+    public static int Paddle2Score = 0;
+    
+	
+	// Update is called once per frame
+	void Update () {
+
+        //updating the score on the scoreboard
+        Scoreboard.text = Paddle1Score.ToString() + " - " + Paddle2Score.ToString();
+        
+	} 
+}
